5ce3b0265eda26bcf1006f8aee1896ca0b64f9ab
Rampawara99 <ramjadhaw1999@gmail.com>
2024-06-05 21:24:36 +0530

    todo routes changes

--- src/routers/todo.routers.js
+++ src/routers/todo.routers.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.route('/add-todo').post(verifyJWT, addTodo);
 router.route('/delete-todo/:_id').delete(verifyJWT, deleteTodo);
-router.route('/update-todo').patch(updateTodo);
-router.route('/get-user-data/:_id').get(getUserTodos);
+router.route('/update-todo').patch(verifyJWT, updateTodo);
+router.route('/get-user-data/:_id').get(verifyJWT, getUserTodos);
 
 export default router;
